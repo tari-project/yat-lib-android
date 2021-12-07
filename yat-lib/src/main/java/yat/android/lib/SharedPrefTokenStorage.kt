68ca019e444fda56bb11370941ceed267502968c
Aleksandr Vakhtin <alexandr@tari.com>
2021-12-07 15:15:29 +0300

    Added basic apis
    Signed-off-by: Aleksandr Vakhtin <alexandr@tari.com>

---
+++ yat-lib/src/main/java/yat/android/lib/SharedPrefTokenStorage.kt
@@ -0,0 +1,21 @@
+package yat.android.lib
+
+import android.content.SharedPreferences
+import com.yatlabs.yat.infrastructure.TokenStorage
+
+class SharedPrefsTokenStorage(private val prefs: SharedPreferences): TokenStorage {
+
+    override var accessToken: String?
+        get() = prefs.getString("access_token", null)
+        set(value) = prefs.edit().run {
+            putString("access_token", value)
+            apply()
+        }
+
+    override var refreshToken: String?
+        get() = prefs.getString("refresh_token", null)
+        set(value) = prefs.edit().run {
+            putString("refresh_token", value)
+            apply()
+        }
+}
